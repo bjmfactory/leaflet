12876b3a4b087878ae2ca662d5353845ff5a3e56
Ben Johnson <ben@bjmfactory.com>
2015-05-01 11:35:16 -0500

    switches to use mapbox.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-var map = L.map('map').setView([30.2500, -97.7500], 13);
+L.mapbox.accessToken = 'pk.eyJ1IjoiYmptZmFjdG9yeSIsImEiOiJtcmx4TVNJIn0.1KjiOWWp8NsVJS1P9JEF2Q';
 
-L.tileLayer('http://{s}.tiles.mapbox.com/v4/mapbox.run-bike-hike/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmptZmFjdG9yeSIsImEiOiJtcmx4TVNJIn0.1KjiOWWp8NsVJS1P9JEF2Q', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
-}).addTo(map);
+var map = L.mapbox.map('map', 'bjmfactory.79138904').setView([30.2700, -97.7550], 12);
+
