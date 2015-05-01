a9a1415e8cd0094ec48900f10bac993c69be8dc6
Ben Johnson <ben@bjmfactory.com>
2015-05-01 12:08:25 -0500

    adds moving marker

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiYmptZmFjdG9yeSIsImEiOiJtcmx4TVNJIn0.1KjiOWWp8NsVJS1P9JEF2Q';
 
-var map = L.mapbox.map('map', 'bjmfactory.79138904').setView([30.2700, -97.7550], 12);
+var map = L.mapbox.map('map', 'bjmfactory.79138904')
+  .setView([29, -26], 2);
+
+var marker = L.marker([30, -97], {
+  icon: L.mapbox.marker.icon({
+    'marker-color': '#f86767'
+  })
+});
+
+var t = 0;
+window.setInterval(function(){
+  marker.setLatLng(L.latLng(
+    Math.cos(t * 0.5) * 50,
+    Math.sin(t) * 50));
+  t += 0.01;
+}, 50);
+
+marker.addTo(map);
 
